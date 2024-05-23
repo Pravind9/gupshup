444bae43ab70c00412cd06993db51b2265245d38
shiv <shiv@psrc.in>
2024-05-23 19:45:04 +0530

    Login authentication

--- src/components/unsecure/login/Login.js
+++ src/components/unsecure/login/Login.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../images/logo/logo.png";
 import { Link } from "react-router-dom";
 
 import "./Login.css";
+import { Password } from "@react-login-page/base";
 
 const Login = (props) => {
-    
-    const formSubmit = (event) => {
+
+    const [data, setData] = useState({});
+
+
+    const formSubmit = async (event) => {
         event.preventDefault();
+
+        try {
+            let uri = props.backend + props.create;
+           
+            const resp = await fetch(uri, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }).then((response) => response.json())
+                .then((response) => {
+                    console.log("response ", response);
+                    if(response.status){
+                        console.log("Redirect to Dashboard");
+                    }else{
+                        console.log("Hold to login Page!");
+                    }
+                });
+        } catch (exception) {
+            console.log("Exception while Login ", exception);
+            throw new Error("Internal Server error");
+        }
     }
 
     return (
@@ -22,13 +49,15 @@ const Login = (props) => {
             <form className="p-3 mt-3" method="post" onSubmit={formSubmit}>
                 <div className="form-field d-flex align-items-center">
                     <span className="fas fa-user"></span>
-                    <input type="text" name="userName" id="userName" placeholder="Username" />
+                    <input type="text" name="uid" id="userId"  placeholder="Username" required={true}
+                        onChange={e => setData({ ...data, uid: e.target.value })} />
                 </div>
                 <div className="form-field d-flex align-items-center">
                     <span className="fas fa-key"></span>
-                    <input type="password" name="password" id="pwd" placeholder="Password" />
+                    <input type="password" name="password" id="pwd" placeholder="Password" required={true}
+                        onChange={e => setData({ ...data, password: e.target.value })} />
                 </div>
-                <button className="btn mt-3">Login</button>
+                <button type="submit" className="btn mt-3">Login</button>
             </form>
             <div className="text-center fs-6">
                 <Link to="/forget">Forget password?</Link> or <Link to="/registration">Sign up</Link>
@@ -38,5 +67,9 @@ const Login = (props) => {
 
 }
 
+Login.defaultProps = {
+    backend: "http://localhost:3003",
+    create: "/api/user/login"
+}
 
 export default Login;
